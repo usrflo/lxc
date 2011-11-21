3f2aac4e5a1c7cf03a2af9507fc7a2a5e9c0df83
Sebastien Pahl <seb@dotcloud.com>
2011-11-21 15:58:14 -0800

    imported patch patch_attach_cgroup_4of5_cgroup.c.patch

--- src/lxc/cgroup.h
+++ src/lxc/cgroup.h
@@ -31,4 +31,5 @@ int lxc_cgroup_destroy(const char *name);
 int lxc_cgroup_path_get(char **path, const char *subsystem, const char *name);
 int lxc_cgroup_nrtasks(const char *name);
 int lxc_ns_is_mounted(void);
+int lxc_cgroup_append_task(const char *name, pid_t pid);
 #endif
